18621f9af158a8c775ec7e856827c8db71f96114
carlopizzuto <carlo.pizzutow@gmail.com>
2025-05-07 21:11:09 -0500

    feat/main: creates an index file w header

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+
+
+#define BLOCK_SIZE 512
+
+
+int is_bigendian() {
+    int x = 1;
+    return ((uint8_t *)&x)[0] != 1;
+}
+
+uint64_t reverse_bytes(uint64_t x) {
+    uint8_t dest[sizeof(uint64_t)];
+    uint8_t *source = (uint8_t*)&x;
+
+    for(int c = 0; c < sizeof(uint64_t); c++) {
+        dest[c] = source[sizeof(uint64_t)-c-1];
+    }
+
+    return *(uint64_t *)dest;
+}
+
+uint64_t host_to_be64(uint64_t x) {
+    if (is_bigendian()) {
+        return x;
+    }
+    return reverse_bytes(x);
+}
 
 int main(int argc, char *argv[]) {
-    int choice;
-    char username[20];
-    char password[20];
-    char confirm_password[20];
-
+    // initial input error handling
     if (argc < 3) {
         fprintf(stderr, "Usage: ./main <command> <index_file> [arguments]\n");
         fprintf(stderr, "Valid commands: create, insert, search, load, print, extract\n");
@@ -15,14 +40,50 @@ int main(int argc, char *argv[]) {
     }
 
     const char *command = argv[1];
-    const char *index_file = argv[2];
+    const char *index_file_path = argv[2];
 
     if (strcmp(command, "create") == 0) {
+        printf("creating index file...\n");  
+
+        // check if command is called with correct number of arguments
         if (argc != 3) {
             fprintf(stderr, "Usage: ./main create <index_file>\n");
             exit(EXIT_FAILURE);
         }
-        printf("creating index file...\n");        
+
+        // check if index file already exists
+        if (access(index_file_path, F_OK) != -1) {
+            fprintf(stderr, "Index file already exists\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // create index file
+        FILE *index_file = fopen(index_file_path, "wb");
+
+        // check if index file was created successfully
+        if (index_file == NULL) {
+            fprintf(stderr, "Failed to create index file\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // initialize header
+        unsigned char buf[BLOCK_SIZE] = {0};           // zero‑filled
+        memcpy(buf+0x00, "4348PRJ3", 8);               // magic num
+        *(uint64_t*)(buf+0x08) = host_to_be64(0);           // root = 0  (empty)
+        *(uint64_t*)(buf+0x10) = host_to_be64(1);           // next = 1  (block #1)
+
+        // write header to index file
+        if (fwrite(buf, BLOCK_SIZE, 1, index_file) != 1) {
+            fprintf(stderr, "Failed to write header to index file\n");
+            exit(EXIT_FAILURE);
+        }
+        fflush(index_file);
+
+        // close index file
+        fclose(index_file);
+
+        // print success message
+        printf("index file created successfully\n");
     }
     else if (strcmp(command, "insert") == 0) {
         if (argc != 5) {
